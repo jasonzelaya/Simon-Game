e55ebe1eb5a475d18f9384dc9e7b070cd486104d
FirstWisdom <jason.zelaya21@yahoo.com>
2019-08-27 10:24:20 -0700

    code added to styles.css and index.js

--- js/index.js
+++ js/index.js
@@ -14,12 +14,22 @@
 var btnColors = ['red', 'green', 'blue', 'yellow'];
 // Game generated pattern for the user to match
 var gamePattern = [];
-// User input patterns
+// User click patterns
 var userPattern = [];
 
 
-
 // ------------JQUERYS---------------
+// Detect if a button is clicked
+$('.btn').click(function() {
+  // Store the id of the button that was clicked into a variable
+  var userChosenColor = $(this).attr('id');
+  //  Add the value of userChosenColor to the end of userPattern
+  userPattern.push(userChosenColor);
+  // Confirm it is working properly in Chrome Developer Tools
+  console.log(userPattern);
+  // Play an audio file relative to the value of userChosenColor
+  playAudio(userChosenColor);
+});
 
 
 
@@ -32,7 +42,7 @@ function nextColor() {
     var randomColor = btnColors[randomNumber];
     // Add randomColor to gamePattern
     gamePattern.push(randomColor);
-    
+
     // Create a flash animation
     $("." + randomColor).fadeOut(100).fadeIn(100).fadeOut(100);
     // Play an audio file relative to the value of randomColor
@@ -40,10 +50,21 @@ function nextColor() {
 
 }
 
+// Add a box-shadow animation on button clicks
+function animateBtnClick(colorClicked) {
+  // Add the clicked class to the button that was clicked
+  $('#' + colorClicked).addClass('clicked');
+
+  // Remove the clicked class after 100 milliseconds
+  setTimeout(function() {
+    $('#' + colorClicked).removeClass('clicked');
+  }, 100);
+}
+
 
 // Play an audio file
 function playAudio(color) {
   // Create a new Audio object for each color
-  var audio = new Audio('sounds/' + randomColor + '.mp3')
+  var audio = new Audio('sounds/' + color + '.mp3')
   audio.play();
 }
